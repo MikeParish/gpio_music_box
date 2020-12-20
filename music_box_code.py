522d956ae76b24b2701509c90067b565883c74b3
Mike Parish <michaeljparish@gmail.com>
2020-12-19 19:32:52 -0500

    Update music_box_code.py
    Added comments about buffer size and pygame 2.0.0 update

--- music_box_code.py
+++ music_box_code.py
@@ -1,6 +1,13 @@
 import pygame
 from gpiozero import Button
 
+#if using pygame version < 2.0.0
+#pre-initialize pygame.mixer with a smaller buffer size to lessen sound lag
+#(pygame 2.0.0 defaults to a buffer size of 512)
+#512 seems to be the smallest buffer size before underrun error occurs
+
+pygame.mixer.pre_init(44100, -16, 2, 512)
+pygame.mixer.init()
 pygame.init()
 
 kick = pygame.mixer.Sound("/home/pi/gpio-music-box/samples/drum_bass_hard.wav")
@@ -9,15 +16,15 @@ hihat = pygame.mixer.Sound("/home/pi/gpio-music-box/samples/drum_cymbal_closed.w
 bass = pygame.mixer.Sound("/home/pi/gpio-music-box/samples/glitch_bass_g.wav")
 glitch = pygame.mixer.Sound("/home/pi/gpio-music-box/samples/glitch_perc5.wav")
 
-#blue/1 is 14
+#blue/1 is gpio 14
 blue_button = Button(14)
-#yellow/2 is 15
+#yellow/2 is gpio 15
 yellow_button = Button(15)
-#green/3 is 18
+#green/3 is gpio 18
 green_button = Button(18)
-#orange/4 is 23
+#orange/4 is gpio 23
 orange_button = Button(23)
-#white/5 is 24
+#white/5 is gpio 24
 white_button = Button(24)
 
 blue_button.when_pressed = kick.play
